8ea5edf96d9e7c5793202eaf01ddf34011491fef
Rodrigue RAINERO <rodriguerainero@gmail.com>
2023-12-12 16:00:00 +0100

    show-pharma

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,46 +1,18 @@
-# class PharmaciesController < ApplicationController
-#   def home
-#     @pharmacies = Pharmacie.find
+class PharmaciesController < ApplicationController
+  before_action :set_pharmacie, only: [:show]
+    def index
+      @pharmacies = Pharmacie.where(user: current_user)
+    end
 
-#     if params[:localisation]
-#       # raise
-#           if params[medicament_id] != "" || params[:pharmacie_id] != ""
-#             @trees = Tree.where(["adress= ? and medicament.nom == ? and stock >= ?", params[:localisation],  params[:medicament], params[:stock]])
-#             # raise
-#           elsif params[:min_size] != "" || params[:max_size] != ""
-#             @trees = Tree.where(["adress= ? and taille >= ? and taille <= ?", params[:localisation],  params[:min_size].to_i, params[:max_size].to_i])
-#             # raise
-#           elsif params[:min_price] != "" || params[:max_price] != "" && params[:min_size] != "" || params[:max_size] != ""
-#             @trees = Tree.where(["adress= ? and taille= ? and price= ?", params[:localisation],  params[:min_size].to_i, params[:max_size].to_i], params[:min_price], params[:max_price])
-#             # raise
-#             # we want to see only available trees
-#           elsif  params[:start_date] != "" && params[:end_date] != ""
-#             @trees = Tree.where(["address= ? ", params[:localisation]])
-#             @trees = @trees.reject do |tree|
-#               tree.bookings.any? do |booking|
-#                 booking_overlaps?(booking, params[:start_date], params[:end_date])
-#               end
-#             end
-#           else
-#             #with only localisation
-#             @pharmacies = Pharmacie.where(address: params[:localisation].downcase)
-#             #  raise
-#           end
-#         end
-# # The `geocoded` scope filters only pharmacie with coordinates
-# @pharmacies = Pharmacie.where(id:@pharmacie.map(&:id))
-# @markers = @pharmacies.geocoded.map do |pharmacie|
-#   {
-#     lat: pharmacie.latitude,
-#     lng: pharmacie.longitude
-#   }
-# end
-#   end
+    def show
+    end
 
-#   def show
-#     # @pharmacies = pharmacie.find(params[:id])
-#   end
-# end
+  private
 
-# if  params [:query].present?
-#   @pharmacies = @pharmacie.where("medicaments.nom ILIKE ?", "%{prams[:query]}")
+  # if  params [:query].present?
+  #   @pharmacies = @pharmacie.where("medicaments.nom ILIKE ?", "%{prams[:query]}")
+  def set_pharmacie
+    @pharmacie = Pharmacie.find(params[:id])
+  end
+
+  end
