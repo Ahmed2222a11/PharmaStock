7d71d566f9dfa21cd42ee7ae36c740576b08c9bc
Rodrigue RAINERO <rodriguerainero@gmail.com>
2023-12-13 08:45:52 +0100

    search-bar-css

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -6,13 +6,15 @@ before_action :set_pharmacie, only: [:show]
     # if params[:medicament] != ""
     #   @pharmacies = Pharmacie.where(["medicament= ?", params[:medicament]])
     # end
-    if !params[:nom_de_medicament].nil? && !params[:quantite].nil?
-    @pharmacies = Pharmacie.joins(stocks: :medicament).where("medicaments.nom ILIKE ? AND stocks.quantite >= ?", "%#{params[:nom_de_medicament]}%", params[:quantite].to_i )
+    @params_quantite = params[:quantite]
+
+    if !params[:nom_de_medicament].nil? && !@params_quantite.nil?
+    @pharmacies = Pharmacie.joins(stocks: :medicament).where("medicaments.nom ILIKE ? AND stocks.quantite >= ?", "%#{params[:nom_de_medicament]}%", @params_quantite.to_i )
     end
   end
 
   def show
-
+    @params_quantite = params[:quantite]
   end
 
 private
