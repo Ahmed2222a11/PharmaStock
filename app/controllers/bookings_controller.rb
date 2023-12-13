661742fb5072f638af4460f46bac3066ba9a5356
Rodrigue RAINERO <rodriguerainero@gmail.com>
2023-12-13 12:27:40 +0100

    maj show booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -28,22 +28,9 @@ end
       end
     else
       render :new, status: :unprocessable_entity
-
-  end
-
-    def create
-      @booking = Booking.new
-      @booking.user = current_user
-      @booking.pharmacie = Pharmacie.find(params[:pharmacie_id])
-      if @booking.save
-        @bookmed = BookingMedicament.new(booking: @booking, medicament: Medicament.find(params[:medicament_id]), quantite: params[:quantite].to_i)
-        if @bookmed.save
-        redirect_to booking_path(@booking)
-        end
-      else
-        render :new, status: :unprocessable_entity
     end
   end
+
 
   def show
     @booking = Booking.find(params[:id])
