44152b5458074a048c790aaf438f628be77027f8
Rodrigue RAINERO <rodriguerainero@gmail.com>
2023-12-15 12:07:41 +0100

    popup-window-map

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -13,4 +13,12 @@ export default class extends Controller {
     this.qty_oneTarget.classList.remove("d-none");
     }
   }
+
+  // addinfowindow() {
+  //   const infowindow = document.querySelectorAll("mapboxgl-popup-content");
+  //   infowindow.forEach((element) => {
+  //     element.classList.remove("d-none");
+  //   });
+  //   console.log(infowindow);
+  // }
 }
